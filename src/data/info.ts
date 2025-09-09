f75f1e0ab0df86a6d5010ef2b204968f9f8fd3ec
Bono Fox <git@bono-fox.de>
2025-09-10 01:32:48 +0200

    💬 Info aktualisiert
    - Arbeit entfernt (Ausstieg zum 31.08.25)
    - weitere Infos hinzugefügt

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,8 +1,20 @@
 import { Info } from "../types/typesInfo";
 
 export const info: Info = {
-  work: {
-    name: "Inverso",
-    url: "https://www.inverso.de",
-  },
+  aboutMe: [
+    { name: "Code Craftsman at heart", emoji: "🤍" },
+    { name: "Engaged", emoji: "💍" },
+    { name: "Atheist", emoji: "⚛️" },
+    { name: "Teetotaler", emoji: "🍻" },
+  ],
+  voluntaryWork: [
+    {
+      name: "Vice Chairperson Jusos Coburg",
+      url: "https://www.instagram.com/jusos_coburg/",
+    },
+  ],
+  hobbies: [
+    { name: "Music", emoji: "🥁" },
+    { name: "Photography", emoji: "📷" },
+  ],
 };
