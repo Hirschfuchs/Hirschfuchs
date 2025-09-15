a3cd9857b03672a8f99c9fa9a54b916d60337812
Bono Fox <git@bono-fox.de>
2025-09-15 16:52:28 +0200

    🔧 Verbleibende Konfiguration für Coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,13 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      exclude: [...(configDefaults.coverage.exclude ?? []), "src/runners/*"],
+      exclude: [
+        ...(configDefaults.coverage.exclude ?? []),
+        "src/runners/**",
+        "src/data/**",
+        "src/enums/**",
+        "src/types/**",
+      ],
     },
   },
 });
